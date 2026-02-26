cb55e1b8b393a2db18db67c58d1018982e2f5cb5
7021833-coder <7021833@spsd.sk.ca>
2026-02-26 13:03:08 -0600

    image demo

---
+++ 3-images-inna-project/sketch.js
@@ -0,0 +1,17 @@
+// Image Demo
+
+let monkeyImg;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  monkeyImg= loadImage("Monkey-Selfie.webp")
+}
+function setup(){
+createCanvas(windowWidth, windowHeight;)
+imageMode(CENTER)
+}
+
+function draw() {
+  background(220);
+  image (monkeyImg, mouseX, mouseY, monkeyImg.width*0.5, monkeyImg.height*0.5);
+}
--- index.md
+++ index.md
@@ -3,6 +3,7 @@
 ## DEMOS
 - [Square](1st-Code)
 - [Scope](2-scope)
+- [Images](3-images-inna-project)
 
 ## Assignments
 - [Interctive-Scene](interactive-scene)
