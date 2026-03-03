ec7c94ae0148eb4c960b7c9528577a02c5d6b03f
7021833-coder <7021833@spsd.sk.ca>
2026-03-02 21:51:17 -0600

    corrections

--- 3-images-inna-project/sketch.js
+++ 3-images-inna-project/sketch.js
@@ -5,13 +5,10 @@ let monkeyImg;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   monkeyImg= loadImage("Monkey-Selfie.webp")
-}
-function setup(){
-createCanvas(windowWidth, windowHeight;)
-imageMode(CENTER)
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
-  image (monkeyImg, mouseX, mouseY, monkeyImg.width*0.5, monkeyImg.height*0.5);
+  image (monkeyImg, mouseX, mouseY, monkeyImg.width*0.3, monkeyImg.height*0.3);
 }
