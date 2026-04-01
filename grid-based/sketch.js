48949e0768023dda3de986fbcac02c96c86dd72a
7021833-coder <7021833@spsd.sk.ca>
2026-04-01 15:25:57 -0600

    Dice Completion(ALMOST)

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -33,21 +33,63 @@ function dice(){
 }
 
 function drawDice(){
+  let x= (width-DICE_SIZE)/2;
+  let y= (height-DICE_SIZE)/2;
+  push();
   stroke(0);
   strokeWeight(2);
   fill ("white");
-  square (829,937,DICE_SIZE);
+  square (x,y,DICE_SIZE);
+  pop();
+
   if (diceNumber===1){
     fill("red");
-    circle ();
+    circle (x+35,y+35,ONEDOT_RADIUS);
   }
-
+  if (diceNumber===2){
+    fill("black");
+    circle (x+35,y+35,TWODOT_RADIUS);
+    circle (x+35,y+35,TWODOT_RADIUS);
+  }
+  if (diceNumber===3){
+    fill("red");
+    circle (x+35,y+35,THREEDOT_RADIUS);
+    circle (x+35,y+35,THREEDOT_RADIUS);
+    circle (x+35,y+35,THREEDOT_RADIUS);
+  }
+  if (diceNumber===4){
+    fill("blue");
+    circle (x+35,y+35,FOURDOT_RADIUS);
+    circle (x+35,y+35,FOURDOT_RADIUS);
+    circle (x+35,y+35,FOURDOT_RADIUS);
+    circle (x+35,y+35,FOURDOT_RADIUS);
+  }
+  if (diceNumber===5){
+    fill("blue");
+    circle (x+35,y+35,FIVEDOT_RADIUS);
+    circle (x+35,y+35,FIVEDOT_RADIUS);
+    circle (x+35,y+35,FIVEDOT_RADIUS);
+    circle (x+35,y+35,FIVEDOT_RADIUS);
+    circle (x+35,y+35,FIVEDOT_RADIUS);
+  }
+  if (diceNumber===6){
+    fill("blue");
+    circle (x+35,y+35,SIXDOT_RADIUS);
+    circle (x+35,y+35,SIXDOT_RADIUS);
+    circle (x+35,y+35,SIXDOT_RADIUS);
+    circle (x+35,y+35,SIXDOT_RADIUS);
+    circle (x+35,y+35,SIXDOT_RADIUS);
+    circle (x+35,y+35,SIXDOT_RADIUS);
+  }
 
 }
 
-function mousePressed(){
-
-
+function mouseClicked() {
+  let x= (width-DICE_SIZE)/2;
+  let y= (height-DICE_SIZE)/2;
+  if (mouseX > x && mouseX < x + DICE_SIZE  && mouseY > y && mouseY < y + DICE_SIZE){
+    dice();
+  }
 }
 
 
